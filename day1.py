d1566175e3abe7bacfc28475a87309f6b08aab75
ThummaluruMahesh <162985714+ThummaluruMahesh@users.noreply.github.com>
2024-03-11 16:29:42 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,43 @@
+#print("hello world")
+
+import this
+print(this)
+
+import this
+import is a keyword which is used to communicated between the python files.
+
+'''
+Bargained Characters
+    controls and data srtucture -> ABC language
+    string and its formats -> from c
+    regular expssions -> pearl
+    exception handling -> module 3
+    magic methods from -> fortron
+'''
+
+
+'''
+what kind of language of python
+1.Object oriented program language
+2.Dynamically typed language
+3.Scripting language
+4.Interepted language
+5.Compiled language
+6.Functional language
+'''
+
+'''
+flavours of language
+>Cpython - Written in C, most common implementation of python
+>Jython - Java implementation of python
+>Ironpython - Implementatiopn of the python programing language which is tightly integrated with NET
+>Brython - Browser python,runs in browser
+>Rubypython - Bridge between python and ruby interpreters
+>Mcropython - Runs in micro controller
+'''
+
+'''
+---->Variables
+>Variable is a name which is used to identify the value of variables
+>Which is stored in a specific memory location
+# Rules to define variables 
