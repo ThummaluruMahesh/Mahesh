3fcf0e7011895e95f337c87b926e9597988a3d5f
ThummaluruMahesh <162985714+ThummaluruMahesh@users.noreply.github.com>
2024-03-21 15:50:47 +0530

    Add files via upload

--- day 10.py
+++ day 10.py
@@ -174,6 +174,7 @@ obj.add(1,2,3)
 7
 1 2 3
 '''
+'''
 class summing:
     def add(self, a=None, b=None, c=None):
         if a!=None and b!=None and c!=None:
@@ -187,6 +188,219 @@ obj = summing()
 obj.add(2)
 obj.add(3,4)
 obj.add(1,2,3)
+
+2
+7
+6
+
+'''
+
+
+# ! ------> Abstraction
+# The process of hiding the implimentation details is adstraction
+
+
+# ? Eg:1
+
+'''
+from abc import ABC, abstractmethod
+class shapes(ABC):
+    @abstractmethod
+    def sides(self):
+        print("All shapes have sides except circle")
+
+class triangle(shapes):
+    def triangle_sides(self):
+        print("3 sides")
+
+    def name(self):
+        print("I am triangle")
+
+    def sides(self):
+        pass
+
+class square(shapes):
+    def sqaure(self):
+        print("4 sides")
+
+tr = triangle()
+tr.triangle_sides()
+tr.name()
+
+3 sides
+I am triangle
+
+'''
+
+# ! Rules to define abstract class1
+# 1.) Abstract class cannot be instantiated
+# 2.) from abc import ABC, abstract method
+# 3.) sub class the normal class with ABC
+# 4.) convert the normal method inside the abstract  class to abstract
+# 5.) all the child classes have to be sub classed with abstract class
+# 6.) the abstract method should be present in the child classes
+
+
+
+# Eg:2
+# super()
+'''
+from abc import ABC, abstractmethod
+class c1(ABC):
+    @abstractmethod
+    def m1 (self):
+        print("This is abstract class")
+
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print(" I am child 1")
+
+    def m1(self):
+        pass
+
+class2 = c2()
+class2.m2()
+
+This is abstract class
+ I am child 1
+'''
+
+
+# * Eg:3
+'''
+from abc import ABC, abstractmethod
+class password(ABC):
+    @abstractmethod
+    def pwd(self):
+        pswd = "sidd1994$"
+        return pswd
+
+class login(password):
+    def validate(self,name,passwrd):
+        if super().pwd() == passwrd:
+            print("welcome", name, "!!")
+            print(" Login succesfull")
+        else:
+            print("Please check the password")
+
+    def pwd(self):
+        pass
+
+
+login = login()
+name = input("Enter the name: ")
+pwd = input("Enter the password: ")
+login.validate(name, pwd)
+
+Enter the name: Mahesh
+Enter the password: 123456
+Please check the password
+123456
+123456
+
+'''
+# ! Encapsulation
+
+'''
+class car:
+    name = "BMW" # private variable
+
+c1 = car()
+print(c1.name)
+c1.name = "audi"
+print(c1.name)
+
+BMW
+audi
+
+'''
+
+
+# * ---> Eg:2
+# ? Accessing private date outside the class
+'''
+class c1:
+    __phone = '9876543210'
+
+    def display(self):
+        print(self.__phone)
+c = c1()
+c.display()
+
+9876543210
+
+'''
+
+# * ------> Eg:3
+# ? declare private method
+'''
+class class1:
+    def __m1(self):
+        print("I am private method")
+
+    def __init__(self):
+        self.__m1()
+
+c = class1()
+#c.__m1() # error
+
+I am private method
+
+'''
+
+# ? nested class
+'''
+class class1:
+    class class2:
+        name = "mahi"
+
+        def display(self):
+            print(self.name)
+    obj1 = class2()
+
+
+obj = class1()
+obj.obj1.display()
+
+mahi
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
